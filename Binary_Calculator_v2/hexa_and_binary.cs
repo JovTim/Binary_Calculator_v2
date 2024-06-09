09935d32990e747eaef726320ee7e2f0ea09605f
JovTim <202280218@psu.palawan.edu.ph>
2024-06-09 23:44:03 +0800

    binary_to_hexa for whole

---
+++ Binary_Calculator_v2/hexa_and_binary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using program_splitter;
+using binary_and_decimal;
+
+namespace Binary_Calculator_v2
+{
+    internal class hexa_and_binary
+    {
+        splitter splitter = new splitter();
+        binary_decimal binary_decimal = new binary_decimal();
+        public string binary_to_hexa(string value)
+        {
+            var (binaryWhole, binaryFraction) = splitter.bin_splitter(value);
+            //-----whole----
+            var binaryWholeContainer = new List<string>() { };
+            string binaryTemp = "";
+
+            for (int i = binaryWhole.Length - 1; i >= 0; i--)
+            {
+                if (binaryTemp.Length == 4)
+                {
+                    binaryWholeContainer.Add(binaryTemp);
+                    binaryTemp = "";
+                }
+                binaryTemp = binaryWhole[i] + binaryTemp;
+            }
+
+            if (String.IsNullOrEmpty(binaryTemp) != true)
+            {
+                for (var i = 0; i <= 4; i++)
+                {
+                    if (binaryTemp.Length == 4)
+                    {
+                        binaryWholeContainer.Add(binaryTemp);
+                        break;
+                    }
+                    binaryTemp = "0" + binaryTemp;
+                }
+            }
+
+            string hexadecimalWhole = "";
+            foreach (var i in binaryWholeContainer)
+            {
+                double y = binary_decimal.binary_to_decimal(i);
+                char convert = splitter.hexa_replacer((int)y);
+                hexadecimalWhole = convert.ToString() + hexadecimalWhole;
+            }
+
+            return hexadecimalWhole; // not final yet :)
+        }
+    }
+}
