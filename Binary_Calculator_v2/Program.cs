866b583fb563c3ff9e5e3bf85519496fca3fbc6f
JovTim <202280218@psu.palawan.edu.ph>
2024-06-12 19:19:25 +0800

    added twos complement

--- Binary_Calculator_v2/Program.cs
+++ Binary_Calculator_v2/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using binary_and_decimal;
 using binary_hexa;
 using binary_octal;
+using twos_complement_file;
 
 namespace Binary_Calculator_v2
 {
@@ -17,13 +18,16 @@ namespace Binary_Calculator_v2
             binary_decimal binary_Decimal = new binary_decimal();
             hexa_and_binary hexa_And_Binary = new hexa_and_binary();
             octal_and_binary octal_And_Binary = new octal_and_binary();
+            twos_complement twos_Complement = new twos_complement();
 
 
             //Console.WriteLine(binary_Decimal.decimal_to_binary("75"));
             //Console.WriteLine(hexa_And_Binary.binary_to_hexa("01111010111.101"));
             //Console.WriteLine(hexa_And_Binary.hexa_to_binary("1C.4A"));
             //Console.WriteLine(binary_Decimal.binary_to_decimal("1011"));
-            Console.WriteLine(octal_And_Binary.octal_to_binary("715.34"));
+            //  Console.WriteLine(octal_And_Binary.octal_to_binary("715.34"));
+
+            Console.WriteLine(twos_Complement.twosComplement("0111011.011"));
             
         }
     }
---
+++ Binary_Calculator_v2/twos_complement.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// two's complement code is based from geeksforgeeks
+// https://www.geeksforgeeks.org/1s-2s-complement-binary-number/
+// However, I modified the code to accept decimal point
+namespace twos_complement_file
+{
+    internal class twos_complement
+    {
+        private static (int, string) pointCounter(string value)
+        {
+            int index = 0;
+            string binary = "";
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == '.')
+                {
+                    index += value.IndexOf('.');
+                    continue;
+                }
+
+                binary += value[i];
+            }
+
+            return (index, binary);
+
+        }
+
+        private static char flip(char c)
+        {
+            return (c == '0') ? '1' : '0';
+        }
+
+        public string twosComplement(string value)
+        {
+            var (count, binary) = pointCounter(value);
+            String ones = "", twos = "";
+            ones = twos = "";
+
+            int i;
+
+            for (i = 0; i < binary.Length; i++)
+            {
+                ones += flip(binary[i]);
+            }
+
+            twos = ones;
+
+            for (i = binary.Length - 1; i >= 0; i--)
+            {
+                if (ones[i] == '1')
+                {
+                    twos = twos.Substring(0, i) + '0' + twos.Substring(i + 1, twos.Length - (i + 1));
+                }
+                else
+                {
+                    twos = twos.Substring(0, i) + '1' + twos.Substring(i + 1, twos.Length - (i + 1));
+                    break;
+                }
+                
+            }
+
+            if (i == -1)
+            {
+                twos = '1' + twos;
+            }
+
+            string lastBinary = twos.Insert(count, ".");
+
+            return lastBinary;
+        }
+
+    }
+}
