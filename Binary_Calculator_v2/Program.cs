5d279b696a8f8ecaad5424d01a9813e6803d44f3
JovTim <202280218@psu.palawan.edu.ph>
2024-06-12 16:54:26 +0800

    added binary to octal

--- Binary_Calculator_v2/Program.cs
+++ Binary_Calculator_v2/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using binary_and_decimal;
 using binary_hexa;
+using binary_octal;
 
 namespace Binary_Calculator_v2
 {
@@ -13,33 +14,16 @@ namespace Binary_Calculator_v2
 
         static void Main(string[] args)
         {
-            // Search How to Convert string number into decimal
-            // Search How to return list from method
-            /*
-            string binary = "1011.11";
-            var whole = new List<decimal>() { };
-
-            foreach(char c in binary)
-            {
-                if (c == '.')
-                {
-                    break;
-                }
-                whole.Add(Convert.ToDecimal(c.ToString()));
-            }
-
-            foreach (var a in whole)
-            {
-                Console.Write(a);
-            }
-            */
             binary_decimal binary_Decimal = new binary_decimal();
             hexa_and_binary hexa_And_Binary = new hexa_and_binary();
+            octal_and_binary octal_And_Binary = new octal_and_binary();
+
 
             //Console.WriteLine(binary_Decimal.decimal_to_binary("75"));
             //Console.WriteLine(hexa_And_Binary.binary_to_hexa("01111010111.101"));
-            Console.WriteLine(hexa_And_Binary.hexa_to_binary("1C.4A"));
+            //Console.WriteLine(hexa_And_Binary.hexa_to_binary("1C.4A"));
             //Console.WriteLine(binary_Decimal.binary_to_decimal("1011"));
+            Console.WriteLine(octal_And_Binary.binary_to_octal("01110111.1101"));
             
         }
     }
---
+++ Binary_Calculator_v2/octal_and_binary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using program_splitter;
+using binary_and_decimal;
+
+namespace binary_octal
+{
+    internal class octal_and_binary
+    {
+        splitter splitter = new splitter();
+        binary_decimal binary_decimal = new binary_decimal();
+
+        public string binary_to_octal(string value)
+        {
+            var (binaryWhole, binaryFraction) = splitter.bin_splitter(value);
+            //----Whole----
+            var binaryWholeContainer = new List<string>() { };
+            string binaryTemp = "";
+
+            for (int i = binaryWhole.Length  - 1; i >= 0; i--)
+            {
+                if (binaryTemp.Length == 3)
+                {
+                    binaryWholeContainer.Add(binaryTemp);
+                    binaryTemp = "";
+                }
+                binaryTemp = binaryWhole[i] + binaryTemp;
+            }
+
+            if (String.IsNullOrEmpty(binaryTemp) != true)
+            {
+                for (var i = 0; i <= 3; i++)
+                {
+                    if (binaryTemp.Length == 4)
+                    {
+                        binaryWholeContainer.Add(binaryTemp);
+                        break;
+                    }
+                    binaryTemp = "0" + binaryTemp;
+                }
+            }
+
+            string octalWhole = "";
+            foreach (string i in binaryWholeContainer)
+            {
+                double y = binary_decimal.binary_to_decimal(i);
+                octalWhole = y.ToString() + octalWhole;
+            }
+
+            //----Fraction----
+            var binaryFractionContainer = new List<string>() { };
+            binaryTemp = "";
+
+            foreach (var i in binaryFraction)
+            {
+                if (binaryTemp.Length == 3)
+                {
+                    binaryFractionContainer.Add(binaryTemp);
+                    binaryTemp = "";
+                }
+                binaryTemp += i;
+            }
+
+            if (String.IsNullOrEmpty(binaryTemp) != true)
+            {
+                for(int i = 0; i <= 4; i++)
+                {
+                    if (binaryTemp.Length == 3)
+                    {
+                        binaryFractionContainer.Add(binaryTemp);
+                        break;
+                    }
+                    binaryTemp += "0";
+                }
+            }
+
+            string octalFraction = ".";
+            foreach (var i in binaryFractionContainer)
+            {
+                double x = binary_decimal.binary_to_decimal(i);
+                octalFraction += x.ToString();
+            }
+
+            return octalWhole + octalFraction;
+        }
+    }
+}
